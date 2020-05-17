c6b4b55c692070b627275380001166ec0341af3d
Archana <archana.vellanki95@msitprogram.net>
2020-05-17 18:29:14 +0530

    Todo Done

--- App.js
+++ App.js
@@ -1,132 +1,218 @@
-import React, {Component, useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList, ScrollView, CheckBox } from 'react-native';
-// import {DatePicker, Container, Content} from 'native-base';
-import DatePicker from 'react-native-datepicker'
-// import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { Component, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  CheckBox,
+} from "react-native";
+import DatePicker from "react-native-datepicker";
+import { AntDesign } from "@expo/vector-icons";
 
 function Task(props) {
-  // const [text, setText] = useState('');
-  console.log(props.dueDate);
-  return (<Text> {props.name}, {props.dueDate},
-      <Button title="Delete Task" onPress={() => { props.onDeleteTask(props.id) }} />
-  </Text>);
+  const [isChecked, setChecked] = useState(false);
+  const date = new Date();
+  return (
+    <Text>
+      <CheckBox
+        value={isChecked}
+        style={styles.checkbox}
+        onValueChange={() => {
+          setChecked(!isChecked);
+        }}
+      />
+      <Text style={isChecked ? styles.strikedTaskContent : styles.taskContent}>
+        {" "}
+        {props.name},{" "}
+        {props.dueDate
+          ? props.dueDate
+          : date.getDate() +
+            "-" +
+            (date.getMonth() + 1) +
+            "-" +
+            date.getFullYear()}{" "}
+      </Text>
+      <AntDesign
+        size={20}
+        color="maroon"
+        name="minuscircle"
+        onPress={() => {
+          props.onDeleteTask(props.id);
+        }}
+      />
+    </Text>
+  );
 }
 
 class TodoList extends Component {
   constructor(props) {
-      super(props);
-      this.state = { list: props.list };
-      this.handleDeleteTask = this.handleDeleteTask.bind(this);
-      this.handleAddTask = this.handleAddTask.bind(this);
+    super(props);
+    this.state = { list: props.list };
+    this.handleDeleteTask = this.handleDeleteTask.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
   }
 
   handleDeleteTask(id) {
-      console.log("Delete task clicked", id);
-      // let list = this.state.list;
-      this.state.list = this.state.list.filter(task => {
-          if (task.id != id)
-              return task;
-
-      })
-      this.setState({ list: this.state.list })
+    this.state.list = this.state.list.filter((task) => {
+      if (task.id != id) return task;
+    });
+    this.setState({ list: this.state.list });
   }
 
-  handleAddTask(task) {
-      console.log("add task clicked");
-      this.state.list.push(task);
-      this.setState({ list: this.state.list })
-  }
+  handleAddTask = (task) => {
+    this.state.list.push(task);
+    this.setState({ list: this.state.list });
+  };
   render() {
-      return (
-          <View>
-              <Text>TODO List</Text>
-              <TaskNameForm onAddTask={this.handleAddTask} />
-              <ScrollView>
-                  {this.state.list.map((t) =>
-                  <Task key={t.id} name={t.name} dueDate={t.dueDate} onDeleteTask={this.handleDeleteTask} id={t.id} />)}
-              </ScrollView>
-              {/* {console.log(this.state.list)} */}
-          </View>
-      );
+    return (
+      <View>
+        <Text style={styles.header}>TODO List</Text>
+        <TaskNameForm onAddTask={this.handleAddTask} />
+        <View style={styles.list}>
+          <ScrollView>
+            {this.state.list.map((t) => (
+              <View style={styles.eachTask}>
+                <Task
+                  name={t.name}
+                  dueDate={t.dueDate}
+                  onDeleteTask={this.handleDeleteTask}
+                  id={t.id}
+                />
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+    );
   }
 }
 
 class TaskNameForm extends Component {
   constructor(props) {
-      super(props);
-      this.state = { value: '', date: '' };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-
-  handleSubmit(event) {
-      // const taskList = this.props.taskList;
-      // create a task object
-      event.preventDefault();
-      console.log("handleSubmit");
-      console.log(this.state.date);
-      const task = {
-          id: Date.now(), name: this.state.value,
-          dueDate: this.state.date
-      };
-      //clearing the input after submit
-      this.setState({ value: ''})
-      // add the task object to the task list
-      this.props.onAddTask(task);
+    super(props);
+    this.state = { value: "", date: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-      // code to set the state of the component
-      console.log("Handle change");
-      this.setState({ value: event.target.value });
-  }
+  handleSubmit = () => {
+    const task = {
+      id: Date.now(),
+      name: this.state.value,
+      dueDate: this.state.date,
+    };
+    this.setState({ value: "" });
+    this.props.onAddTask(task);
+  };
+
+  handleChange = (text) => {
+    this.setState({ value: text });
+  };
 
   handleDateChange = (date) => {
-    // code to set the state of the component
-    console.log(date);
-    this.setState({date: date})
-}
+    this.setState({ date: date });
+  };
 
   render() {
-      return (
-          <View >
-              {/* <View> */}
-              <TextInput onChange={this.handleChange} placeholder="Write the task.." value={this.state.value} />
-            <DatePicker
-            date= {this.state.date}
-            placeHolder="Due Date:"
-            mode="date"
-            format="DD-MM-YYYY"
-            minDate="01-01-2020"
-            maxDate="01-01-2021"
-            display="default"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            onDateChange= {this.handleDateChange}
-            />
-              <Button title="Add Task" onPress={this.handleSubmit}/>
-          </View>
-      );
+    return (
+      <View style={styles.form}>
+        <TextInput
+          style={styles.txtInput}
+          onChangeText={this.handleChange}
+          placeholder="New Task Name..."
+          value={this.state.value}
+        />
+        <DatePicker
+          date={this.state.date}
+          placeHolder="Due Date:"
+          mode="date"
+          format="DD-MM-YYYY"
+          minDate="01-01-2020"
+          maxDate="01-01-2021"
+          display="default"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          onDateChange={this.handleDateChange}
+        />
+        <Button title="Add" onPress={this.handleSubmit} />
+      </View>
+    );
   }
 }
 
 export default function App() {
-    
   return (
     <View style={styles.container}>
-      <TodoList list={[]} />
+      <TodoList list={[{ name: "Project", dueDate: "23-12-2020" }]} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      padding:20,
+    padding: 20,
     flex: 3,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+  },
+  formName: {
+    fontSize: 20,
+    backgroundColor: "lavenderblush",
+    borderRadius: 100 / 12,
+    color: "darkmagenta",
 
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    marginBottom: 10,
+  },
+  del: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    color: "red",
+  },
+  header: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 40,
+    padding: 20,
+    height: 100,
+    color: "darkmagenta",
+  },
+  form: {
+    flexDirection: "row",
+    fontSize: 10,
+    justifyContent: "space-between",
+    paddingBottom: 10,
+    marginBottom: 10,
+    borderBottomWidth: 2,
+    borderColor: "palevioletred",
+  },
+  txtInput: {
+    width: 170,
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: "darkgrey",
+  },
+  checkbox: {
+    paddingTop: 10,
+    height: 20,
+  },
+  list: {
+    marginTop: 20,
+    backgroundColor: "mistyrose",
+    borderRadius: 100 / 12,
+  },
+  eachTask: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "palevioletred",
+  },
+  taskContent: {
+    fontSize: 18,
+  },
+  strikedTaskContent: {
+    fontSize: 18,
+    textDecorationLine: "line-through",
   },
 });
